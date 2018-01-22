45096b7428c015f15b6090229219373fd9982cdf
jlwgong <jlwgong@gmail.com>
2018-01-22 11:43:55 +0600

    Section A Tic Tact Toe
    January 22, 2018

---
+++ group_projects/Section_A/tic_tac_toe_sectionA.py
@@ -0,0 +1,40 @@
+board1 = range(1,10)
+a1=" "
+a2=" "
+a3=" "
+b1=" "
+b2=" "
+b3=" "
+c1=" "
+c2=" "
+c3=" "
+def board():
+    
+    print a1 + "|" + a2 + "|" + a3
+    print "-----"
+    print b1 + "|" + b2 + "|" + b3
+    print "-----"
+    print c1 + "|" + c2 + "|" + c3
+    
+board()
+def input4players():
+    print "player1: choose the coordinate of the X"
+    vvod = raw_input("")
+    if (vvod = 1):
+        a1="X"
+    if (vvod = 2):
+        a2="X"
+    if (vvod = 3):
+        a3="X"
+    if (vvod = 4):
+        b1="X"
+    if (vvod = 5):
+        b2="X"
+    if (vvod = 6):
+        b3="X"
+    if (vvod = 7):
+        c1="X"
+    if (vvod = 8):
+        c2="X"    
+    if (vvod = 9):
+        c3="X"
